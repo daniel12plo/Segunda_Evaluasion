1540a1d077a8287bd66ed7d7ec4080d395ec9900
daniel12plo <43232637+daniel12plo@users.noreply.github.com>
2019-02-21 09:45:17 +0100

    Add files via upload

--- tabla.cpp
+++ tabla.cpp
@@ -2,6 +2,18 @@
 int main(){
     int numero;
     int i;
-    std::cout<<"Que tabla desea hacer? (1-10)"
-    std::cin>> numero
-    for(i=1<=10;i++)}
+    char salir;
+    std::cout<<"Que tabla desea hacer? (0-10)";
+    std::cin>>numero;
+    for(i=1;i<=10;i++){
+    std::cout<<i;
+    std::cout<<" X ";
+    std::cout<<numero;
+    std::cout<<" = ";
+    std::cout<<i*numero;
+    std::cout<<"\n";
+}
+std::cout<<"\nToca cualquier tecla para salir";
+    std::cin>>salir;
+    return 0;
+}
